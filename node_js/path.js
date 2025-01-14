5442986f0cbd50676c2a416c075d087b41626ec0
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-14 14:22:43 +0600

    os module and path module

---
+++ node_js/path.js
@@ -0,0 +1,5 @@
+const path=require('path');
+const extensionName=path.extname("path.js")
+console.log(extensionName);
+const join=path.join(__dirname+"/views");
+console.log(join)
--- node_js/read_fille.js
+++ node_js/read_fille.js
@@ -1,8 +1,9 @@
-const fs=require('fs');
-fs.appendFile('demo1.txt','utf-8',(err,data)=>{
-    if(err){
-        console.log("error")
-    }else{
-        console.log("successfull");
+const fs = require('fs');
+
+fs.readFile('demo1.txt', 'utf-8', (err,data) => {
+    if (err) {
+        console.log("error");
+    } else {
+        console.log("successful");
     }
-})
+});
